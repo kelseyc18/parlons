031552e1a5b83cedf7e74bda247083e41c348d8b
Kelsey Chan <kelseykchan@gmail.com>
2017-02-18 13:45:02 -0800

    Updated UI
    * Grid layout
    * Matching algorithm
    * Link to profiles

--- webapp.py
+++ webapp.py
@@ -3,6 +3,7 @@ from flask import session as login_session
 from flask_oauth import OAuth
 from model import *
 import json
+import operator
 
 SECRET_KEY = 'development key'
 DEBUG = True
@@ -22,7 +23,7 @@ facebook = oauth.remote_app('facebook',
     authorize_url='https://www.facebook.com/dialog/oauth',
     consumer_key=FACEBOOK_APP_ID,
     consumer_secret=FACEBOOK_APP_SECRET,
-    request_token_params={'scope': ['email', 'user_likes', 'user_hometown', 'user_location']}
+    request_token_params={'scope': ['email', 'user_likes', 'user_hometown', 'user_location', 'user_friends']}
 )
 
 engine = create_engine('sqlite:///database.db')
@@ -41,7 +42,7 @@ def my_profile():
         return redirect(url_for('login'))
     else:
         currUser = session.query(User).filter_by(facebook_id=login_session['facebook_id']).one()
-        my_languages = session.query(LanguageAssociation).filter_by(user=currUser).all()
+        my_languages = [assoc.language for assoc in session.query(LanguageAssociation).filter_by(user=currUser).all()]
         all_languages = session.query(Language).all()
         all_languages.sort(key=lambda language: language.name)
         return render_template('index.html', user=currUser, \
@@ -63,9 +64,10 @@ def facebook_authorized(resp):
             request.args['error_description']
         )
     login_session['oauth_token'] = (resp['access_token'], '')
-    me = facebook.get('/me?fields=id,name,languages,email,location,hometown')
+    me = facebook.get('/me?fields=id,name,languages,email,location,hometown,friends{languages,name}')
     login_session['facebook_id'] = me.data['id']
 
+    friends = None
     # Create new User if necessary
     if session.query(User).filter_by(facebook_id=me.data['id']).first() is None:
         my_languages = []
@@ -75,7 +77,7 @@ def facebook_authorized(resp):
                 currLanguage = session.query(Language).filter_by(language_id=language['id']).first()
                 # Create new Language if necessary
                 if currLanguage is None:
-                    currLanguage = Language(name=language['name'], id=language['id'])
+                    currLanguage = Language(name=language['name'], language_id=language['id'])
                     session.add(currLanguage)
                     session.commit()
                 my_languages.append(currLanguage)
@@ -109,11 +111,45 @@ def logout():
     return redirect(url_for('index'))
 
 
+def get_languages(user):
+    return [assoc.language for assoc in session.query(LanguageAssociation).filter_by(user=user).all()]
+
+
+def get_learning_languages(user):
+    return [assoc.language for assoc in session.query(LearningLanguageAssociation).filter_by(user=user).all()]
+
+
 @app.route('/matches')
 def matches():
     # TODO: does not work. need to rewrite matching algorithm
+    me = session.query(User).filter_by(facebook_id=login_session['facebook_id']).one()
+    my_languages = get_languages(me)
+    my_languages_to_learn = get_learning_languages(me)
+    
     users = session.query(User).all()
-    return ', '.join([user.name for user in users])
+    user_dict = {}
+    for user in users:
+        score = 0
+        if user is not me:
+            user_languages = get_languages(user)
+            user_languages_to_learn = get_learning_languages(user)
+
+            # Check common language
+            score += len(set(my_languages) & set(user_languages)) * 10
+
+            # If User knows a language you want to learn
+            score += len(set(my_languages_to_learn) & set(user_languages)) * 20
+
+            # If you know a language User wants to learn
+            score += len(set(my_languages) & set(user_languages_to_learn)) * 5
+
+            # TODO: mutual friends
+            # TODO: location
+            user_dict[user] = score
+    sorted_users = []
+    for u in sorted(user_dict, key=user_dict.get, reverse=True):
+        sorted_users.append((u, user_dict[u]))
+    return json.dumps([{'name': user.name, 'score': score, 'facebook_id': user.facebook_id} for (user, score) in sorted_users])
 
 
 @app.route('/languagesToLearn')
