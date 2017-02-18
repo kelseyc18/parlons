0c4da275a54d31a13d6a9bf523cc400e6dc8df9b
Kelsey Chan <kelseykchan@gmail.com>
2017-02-18 06:41:23 -0800

    Set up profile interface

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, flash
 from flask import session as login_session
 from flask_oauth import OAuth
 from model import *
@@ -34,6 +34,17 @@ session = DBSession()
 def index():
     return redirect(url_for('login'))
 
+@app.route('/me')
+def my_profile():
+    if 'oauth_token' not in login_session or login_session['oauth_token'] == '':
+        return redirect(url_for('login'))
+    else:
+        currUser = session.query(User).filter_by(facebook_id=login_session['facebook_id']).one()
+        my_languages = session.query(LanguageAssociation).filter_by(user=currUser).all()
+        all_languages = session.query(Language).all()
+        all_languages.sort(key=lambda language: language.name)
+        return render_template('index.html', user=currUser, \
+            my_languages=my_languages, all_languages=all_languages)
 
 @app.route('/login')
 def login():
@@ -52,6 +63,7 @@ def facebook_authorized(resp):
         )
     login_session['oauth_token'] = (resp['access_token'], '')
     me = facebook.get('/me?fields=id,name,languages,email')
+    login_session['facebook_id'] = me.data['id']
 
     # Create new User if necessary
     if session.query(User).filter_by(facebook_id=me.data['id']).first() is None:
@@ -74,7 +86,7 @@ def facebook_authorized(resp):
             session.add(languageAssociation)
             session.commit()
     currUser = session.query(User).filter_by(facebook_id=me.data['id']).one()
-    return render_template('index.html', user=currUser)
+    return redirect(url_for('my_profile'))
     # return 'Logged in as id=%s name=%s languages=%s redirect=%s' % \
     #     (me.data['id'], me.data['name'], me.data['languages'], request.args.get('next'))
 
@@ -82,6 +94,40 @@ def facebook_authorized(resp):
 @facebook.tokengetter
 def get_facebook_oauth_token():
     return login_session.get('oauth_token')
+
+
+@app.route('/logout')
+def logout():
+    login_session.pop('oauth_token')
+    return redirect(url_for('index'))
+
+
+@app.route('/matches')
+def matches():
+    if session.query(User).first() is not None:
+        # TODO: actually return matches
+        return session.query(User).first().name
+    return ''
+
+
+@app.route('/languagesToLearn')
+def languages_to_learn():
+    user = session.query(User).filter_by(facebook_id=login_session['facebook_id']).one()
+    languages = [assoc.language for assoc in user.learningLanguages]
+    return ', '.join([language.name for language in languages])
+
+
+@app.route('/updateLearn', methods = ['POST'])
+def update_learn():
+    if request.method == 'POST':
+        user = session.query(User).filter_by(facebook_id=login_session['facebook_id']).one()
+        language_ids = request.form.getlist('learnLanguages')
+        session.query(LearningLanguageAssociation).filter_by(user=user).delete()
+        for language_id in language_ids:
+            language = session.query(Language).filter_by(id=language_id).one()
+            session.add(LearningLanguageAssociation(language=language, user=user))
+            session.commit()
+        return redirect(url_for('my_profile'))
 
 
 if __name__ == '__main__':
